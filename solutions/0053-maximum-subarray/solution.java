6cc990ee9c6ed1f351d8513aefa754ff141ede48
tarushivishwakarma <tarushivishwakarma22@gmail.com>
2025-10-06 05:01:38 +0000

    [LeetCode Bulk Sync] Runtime - 0 ms (100.00%), Memory - 57.4 MB (99.93%)

---
+++ solutions/0053-maximum-subarray/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int max=nums[0];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max=(sum>max)?sum:max;
+            if(sum<0)
+            sum=0;
+        }
+        return max;
+    }
+}

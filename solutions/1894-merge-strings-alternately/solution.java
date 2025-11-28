d2775e9200325faa0f5c90f287c0ef566f038498
tarushivishwakarma <tarushivishwakarma22@gmail.com>
2025-11-28 04:42:59 +0000

    [LeetCode Bulk Sync] Runtime - 1 ms (95.78%), Memory - 42.9 MB (77.80%)

---
+++ solutions/1894-merge-strings-alternately/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb=new StringBuilder(); 
+        int n1=word1.length();
+         int n2=word2.length();
+          for(int i=0;i<Math.max(n1,n2);i++){ 
+            
+            if(i<n1) 
+            sb.append(word1.charAt(i));
+             if(i<n2) 
+             sb.append(word2.charAt(i));
+              }
+               return sb.toString();
+    }
+}
